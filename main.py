9043f6aface287333e35ca78ce3a57ebd4bca59d
nov05 <student-04-770ae3895125@qwiklabs.net>
2026-03-24 05:14:54 +0000

    Testing Build Trigger

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def main():
     model = {"title":  "Hello Build Trigger."}
     return render_template("index.html", model=model)
  
- 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
     port = int(os.environ.get("PORT", 8080))  # fallback to 8080 locally
