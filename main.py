b92095392c8a1454134d32964a8df0de839f0847
nov05 <1215590+nov05@users.noreply.github.com>
2026-03-26 10:33:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def main():
-#     model = {"title": "Hello DevOps Fans!"}
-#     return render_template('index.html', model=model)
-
 @app.route("/")
 def main():
+#     model = {"title": "Hello DevOps Fans!"}
     model = {"title":  "Hello Build Trigger!"}
     return render_template("index.html", model=model)
  
  
 if __name__ == "__main__":
+    
     # app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
     port = int(os.environ.get("PORT", 8080))  # fallback to 8080 locally
     app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
